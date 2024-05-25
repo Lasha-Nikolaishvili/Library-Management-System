447b9b47a36c87c6e099e1f0449084b20c6b1901
lasha_shala <nikolaishvililashan1@gmail.com>
2024-05-26 03:48:10 +0400

    added login register and some other styling

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
@@ -33,3 +34,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
